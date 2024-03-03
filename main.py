77cc069c35c3c85f9f0801425fa70abc7bc3d487
Chenjinxuu <160599422+Chenjinxuu@users.noreply.github.com>
2024-03-03 06:53:07 +0000

    Final submission
    Much easier than the task that told me to find the sf of the input

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 num = input('Enter a number (decimal only): ')
+num1 = num.replace(".", "")
+
 # type your code here
-
-
+if "." in num == False or num1.isdigit() == False or num.count(".") > 1:
+ num = "error "
+ dp = "error"
+ print("Please follow the instruction given and redo the test.")
+else:
+ num.find(".")
+ dp = len(num[num.find(".")+1:])
+ 
 
 # do not change any code beyond this point
 print('The number', num, 'has', dp, 'decimal places.')
